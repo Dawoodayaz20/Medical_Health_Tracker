0f76230a75e332e291338e1479c34c7aa19f2b08
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-11-01 22:24:52 +0500

    Renamed Notes Page to Medical Records Page.

--- app/(Medicines)/AddMedicines.tsx
+++ app/(Medicines)/AddMedicines.tsx
@@ -22,71 +22,7 @@ export default function AddMedicine() {
   }
 
   return (
-//     <View>
-//     <View className="flex-row p-6 items-center text-center">
-//     <ArrowLeft onPress={(() => router.back())} size={22} color="black"/>
-//     <Text className="ml-20 text-center text-2xl font-bold text-blue-700">Add Medicine</Text>
-//     </View>
-//     <ScrollView className="flex-1 bg-blue-50 p-6">
-//       <View className="gap-4 flex">
-//         <TextInput
-//           placeholder="Medicine Name"
-//           placeholderTextColor="#555"
-//           value={name}
-//           onChangeText={setName}
-//           className="bg-white p-4 rounded-xl border border-gray-200 text-gray-800"
-//         />
-
-//         <TextInput
-//           placeholder="Illness (e.g. Fever, Headache)"
-//           placeholderTextColor="#555"
-//           value={illness}
-//           onChangeText={setIllness}
-//           className="bg-white p-4 rounded-xl border border-gray-200 text-gray-800"
-//         />
-
-//         <TextInput
-//           placeholder="Dosage (optional)"
-//           placeholderTextColor="#555"
-//           value={dosage}
-//           onChangeText={setDosage}
-//           className="bg-white p-4 rounded-xl border border-gray-200 text-gray-800"
-//         />
-
-//         <View className="flex-row items-center bg-white p-4 rounded-xl border border-gray-200">
-//           <Clock size={18} color="#2563eb" />
-//           <TextInput
-//             placeholder="Time (e.g. 8:00 AM)"
-//             placeholderTextColor="#555"
-//             value={time}
-//             onChangeText={setTime}
-//             className="ml-2 flex-1 text-gray-800"
-//           />
-//         </View>
-        
-//         <TextInput
-//           placeholder="Notes (optional)"
-//           placeholderTextColor="#555"
-//           value={detail}
-//           onChangeText={setDetail}
-//           className="bg-white p-4 rounded-xl border border-gray-200 text-gray-800"
-//         />
-//       </View>
-
-//       <TouchableOpacity
-//         onPressIn={(() => saveMedicine())}
-//         onPress={(() => router.back())}
-//         className="bg-blue-600 p-4 rounded-xl mt-8 shadow-md"
-//       >
-//         <Text className="text-white text-center text-lg font-semibold">Save</Text>
-//       </TouchableOpacity>
-//     </ScrollView>
-//     </View>
-//   );
-// }
-
-
-<View style={styles.container}>
+    <View style={styles.container}>
       {/* Header */}
       <View style={styles.header}>
         <ArrowLeft onPress={() => router.back()} size={22} color="black" />
--- app/(Records)/_layout.tsx
+++ app/(Records)/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from "expo-router";
 export default function RecordsLayout() {
     return (
     <Stack>
-        <Stack.Screen name="NotesPage" options={{ headerShown:false }} />
+        <Stack.Screen name="MedicalRecordsPage" options={{ headerShown:false }} />
         <Stack.Screen name="AddNote" options={{ headerShown:false }} />
     </Stack>
 )
